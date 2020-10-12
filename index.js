892c6a6721ad88280919cf3861e41f80dbbdeb97
PabloSanz11 <jpablosang11@gmail.com>
2020-10-12 17:18:43 -0500

    Using basic POST

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-
+const bodyParser = require('body-parser');
 const express = require('express'); //Obtener la libreria
 const app = express(); //Asignar la libreria a app
 const {pokemon} = require('./pokedex.json'); //Importar la base de datos y extraer el elmento especifico
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 /** 
  * Verbos HTTP
  * GET
  * POST
- * PATCH ----> Actualizacion de un dato de un recurso especifico (contraseña)
+ * PATCH ----> Actualizacion de un dato de un recurso especifico (contraseña, id, name, etc)
  * PUT   ----> Actualización de todos los recursos especificados
  * DELETE
 **/
@@ -15,55 +18,48 @@ const {pokemon} = require('./pokedex.json'); //Importar la base de datos y extra
 //req es la petición del cliente navegador, res es la respuesta que nosotros mandaremos
 app.get("/", (req, res, next) =>
 {
-    res.status(200);
-    res.send("Bienvenido al pokedex");
+    return res.status(200).send("Bienvenido al pokedex");
 });
 
-app.get("/pokemon/all", (req, res, next) =>
+//<--------- POST--------------->
+app.post("/pokemon", (req, res, next) =>
 {
-    res.status(200);
-    res.send(pokemon);   
+    return res.status(200).send(req.body); 
+});
+
+
+app.get("/pokemon", (req, res, next) =>
+{
+    return res.status(200).send(pokemon);   
 });
 
 app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) =>
 {
     const id = req.params.id - 1;
 
-    if(id >= 0 || id <= 150)
-    {
-        res.status(200);
-        res.send(pokemon[req.params.id-1]); 
-        
-    }else
-    {
-        res.status(404);
-        res.send("El pokemon no ha sido encontrado");
-    }
+    (id >= 0 || id <= 150) ? res.status(200).send(pokemon[req.params.id-1]) : res.status(404).send("El pokemon no ha sido encontrado");
 
 });
 
-app.get('/pokemon/:name' , (req, res, next) =>
+//Procesar solo aquellas que lleven un texto en la variable [1]
+app.get('/pokemon/:name([A-Za-z]+)' , (req, res, next) =>
 {
     const name = req.params.name;
-
-    for(i = 0; i < pokemon.length; i++)
+    //Es un ciclo donde se pasa un valor, p accede a todos los atributos de la bd [2]
+    const pk = pokemon.filter((p) =>
     {
-        if(pokemon[i].name == name.trim())
-        {
-            res.status(200);
-            res.send(pokemon[i]);
-        }
-    }
+        //Condicion ? si es verdadero : si es falso; [3]
+        return (p.name.toUpperCase() == name.toUpperCase()) && p;
+    });
 
-    res.status(404);
-    res.send("Pokemon no encontrado");
-    
+    //pk es un arreglo con sus posiciones
+    (pk.length > 0) ? res.status(200).send(pk) : res.status(404).send("Pokemon no encontrado");;
+
 });
 
 app.get('/pokemon/:num', (req, res, next) =>
 {
-    res.status(200);
-    res.send(pokemon[req.params.num]);
+    return res.status(200).send(pokemon[req.params.num]);
 });
 
 //Encender el servidor para que escuche
