823e148fe0d74d9c270cab9466dbb7a187d6359a
PabloSanz11 <jpablosang11@gmail.com>
2020-10-14 21:46:57 -0500

    Router, Morgan, Mysql

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const express = require('express'); //Obtener la libreria
 const app = express(); //Asignar la libreria a app
-const {pokemon} = require('./pokedex.json'); //Importar la base de datos y extraer el elmento especifico
+const pokemon = require('./routes/pokemon.js');
 
+
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -21,46 +24,7 @@ app.get("/", (req, res, next) =>
     return res.status(200).send("Bienvenido al pokedex");
 });
 
-//<--------- POST--------------->
-app.post("/pokemon", (req, res, next) =>
-{
-    return res.status(200).send(req.body); 
-});
-
-
-app.get("/pokemon", (req, res, next) =>
-{
-    return res.status(200).send(pokemon);   
-});
-
-app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) =>
-{
-    const id = req.params.id - 1;
-
-    (id >= 0 || id <= 150) ? res.status(200).send(pokemon[req.params.id-1]) : res.status(404).send("El pokemon no ha sido encontrado");
-
-});
-
-//Procesar solo aquellas que lleven un texto en la variable [1]
-app.get('/pokemon/:name([A-Za-z]+)' , (req, res, next) =>
-{
-    const name = req.params.name;
-    //Es un ciclo donde se pasa un valor, p accede a todos los atributos de la bd [2]
-    const pk = pokemon.filter((p) =>
-    {
-        //Condicion ? si es verdadero : si es falso; [3]
-        return (p.name.toUpperCase() == name.toUpperCase()) && p;
-    });
-
-    //pk es un arreglo con sus posiciones
-    (pk.length > 0) ? res.status(200).send(pk) : res.status(404).send("Pokemon no encontrado");;
-
-});
-
-app.get('/pokemon/:num', (req, res, next) =>
-{
-    return res.status(200).send(pokemon[req.params.num]);
-});
+app.use("/pokemon", pokemon); // cada que se detecte al pokemon se manda al js
 
 //Encender el servidor para que escuche
 app.listen(process.env.PORT || 3000, () =>
---
+++ routes/pokemon.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const pokemon = express.Router();
+const db = require('../config/database');
+
+// const pk = require('../pokedex.json').pokemon; Importar la base de datos y extraer el elmento especifico
+
+//<--------- POST--------------->
+pokemon.post("/", (req, res, next) =>
+{
+    return res.status(200).send(req.body); 
+});
+
+
+pokemon.get("/", async (req, res, next) =>
+{
+    const pkmn =  await db.query("SELECT * FROM pokemon"); //Es el query
+    return res.status(200).json(pkmn); //Solo cuando ya viene en formato json 
+});
+
+pokemon.get('/:id([0-9]{1,3})', async (req, res, next) =>
+{
+    const id = req.params.id;
+    const pkmn =  await db.query("SELECT * FROM pokemon WHERE pok_id ="+ id); //Es el query
+
+    if(id >= 0 && id <= 150)
+    {
+       return res.status(200).json(pkmn);
+    }  
+    
+    return res.status(404).send("El pokemon no ha sido encontrado");
+    //(id >= 0 || id <= 150) ? res.status(200).send(pk[req.params.id-1]) : res.status(404).send("El pokemon no ha sido encontrado");
+
+});
+
+//Procesar solo aquellas que lleven un texto en la variable [1]
+pokemon.get('/:name([A-Za-z]+)', async (req, res, next) =>
+{
+    const name = req.params.name;
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name = '" + name + "'"); //Es el query
+
+    try 
+    {
+        if(pkmn.length > 0)
+        {
+            return res.status(200).json(pkmn);
+        }
+
+        return res.status(404).send("Pokemon no encontrado");
+    } catch (error) {
+        console.log(error);
+    }
+    
+    //Es un ciclo donde se pasa un valor, p accede a todos los atributos de la bd [2]
+    /*
+    const pkmn = json(pkmns).filter((p) =>
+    {
+        //Condicion ? si es verdadero : si es falso; [3]
+        return (p.name.toUpperCase() == name.toUpperCase()) && p;
+    });
+
+    //pkmn es un arreglo con sus posiciones
+    if(pkmn.length > 0) 
+    {
+        return res.status(200).json(pkmn);
+    } 
+
+    return res.status(404).send("Pokemon no encontrado");
+    */
+});
+
+module.exports = pokemon; //Exportar cosas y solo una
