7f65433fa985889bd98a5b4beeb414a28fcd59a5
PabloSanz11 <jpablosang11@gmail.com>
2020-11-04 16:30:50 -0600

    JWT

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const user = express.Router();
 const db = require('../config/database');
+const jwt = require('jsonwebtoken');
 
 user.post("/", async (req,res,next) =>
 {
@@ -9,7 +10,7 @@ user.post("/", async (req,res,next) =>
     if(user_name && user_mail && user_password)
     {
         let query = "INSERT INTO user(user_name, user_mail, user_password)";
-        query += `VALUES ('${user_name}', '${user_mail}', '${user_password}')`;
+        query += `VALUES ('${user_name}', '${user_mail}', '${user_password}');`;
         const rows = await db.query(query);
 
         if(rows.affectedRows == 1)
@@ -23,9 +24,36 @@ user.post("/", async (req,res,next) =>
     return res.status(500).json({code: 500, message: "Campos incompletos"});
 });
 
+user.post("/login", async (req, res, next) =>
+{
+    const {user_mail, user_password} = req.body;
+
+    if(user_mail && user_password)
+    {
+        const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}';`;
+        const rows = await db.query(query);
+
+        if(rows.length == 1)
+        {
+            const token = jwt.sign(
+            {
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            }, "debugkey");
+
+            return res.status(200).json({code: 200, message: token});
+        }else
+        {
+            return res.status(401).json({code: 401, message: "Usuario y/o Contraseña incorrectos"});
+        }
+    }
+    
+    return res.status(500).json({code: 500, message: "Campos incompletos"});
+});
+
 user.get("/", async (req, res, next) =>
 {
-    const query = await db.query("SELECT * FROM user");
+    const query = await db.query("SELECT * FROM user;");
     return res.status(200).json({code: 200, message: query});
 });
 
